fed8c1f4645c26ad527c8d20e7e95c2b13296a4b
Joakim Nohlgård <joakim@nohlgard.se>
2024-09-27 12:45:47 +0200

    install-talosctl: change download path to match new shasum files of Talos 1.8

--- admin/install-talosctl.sh
+++ admin/install-talosctl.sh
@@ -17,14 +17,13 @@ esac
 version="$(github_latest_version "${repo_url}")"
 release_url="${repo_url}/releases/download/${version}"
 
-mkdir -p "${tmpdir}/_out"
-curl -LfO --output-dir "${tmpdir}/_out" "${release_url}/${exe}-linux-${altarch}"
 curl -Lf --output-dir "${tmpdir}" --remote-name-all \
   "${release_url}/sha256sum.txt" \
-  "${release_url}/sha512sum.txt"
+  "${release_url}/sha512sum.txt" \
+  "${release_url}/${exe}-linux-${altarch}"
 ( cd "${tmpdir}" && \
   ${sha256sum_cmd} --ignore-missing -c "${tmpdir}/sha256sum.txt" && \
   ${sha512sum_cmd} --ignore-missing -c "${tmpdir}/sha512sum.txt" \
 )
 
-install_exe "${tmpdir}/_out/${exe}-linux-${altarch}" "${exe}"
+install_exe "${tmpdir}/${exe}-linux-${altarch}" "${exe}"
